aaf1d57ba9f286148bfcc3c20b9f9930ac711e19
ansmeer008 <ansmeer008@gmail.com>
2023-03-13 22:27:35 +0900

    initial setting: add Header Component, Layout

---
+++ src/components/Header.js
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <nav>this is Header</nav>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="container"></div>;
+  return <div className="container">안녕 나는 my dibs!</div>;
 }
