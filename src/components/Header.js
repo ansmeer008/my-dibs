173d1f6e217833c24a74b377813a269d124f7660
ansmeer008 <ansmeer008@gmail.com>
2023-03-14 22:18:55 +0900

    feat: Header Component markup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,18 @@
+import classes from "./Header.module.css";
+import Link from "next/link";
+import { TbHandFinger } from "react-icons/tb";
+
 export default function Header() {
-  return <nav>this is Header</nav>;
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <div className={classes.title}>
+          <h1>My Dibs!</h1>
+          <div className={classes.icon}>
+            <TbHandFinger />
+          </div>
+        </div>
+      </Link>
+    </header>
+  );
 }
