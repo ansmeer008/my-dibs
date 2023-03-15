c474ec33794a6fef87df0f3ceccbc5404370acba
ansmeer008 <ansmeer008@gmail.com>
2023-03-15 21:20:55 +0900

    feat: add All Button

--- src/components/TagButton.js
+++ src/components/TagButton.js
@@ -1,11 +1,9 @@
 import classes from "./TagButton.module.css";
 
 export default function TagButton({ data }) {
-  //클릭한 버튼의 이름을... 홈 페이지에 전달해서, map을 돌릴 때 filter로 돌려야 한다...
-  //클릭을 했을 때 서버에서 이미 filter한 데이터를 주면 좋겠지만,,, 가능하지 않을 수도 있으므로.,
-
   return (
     <div className={classes.container}>
+      <button>All</button>
       <button>Clothes </button>
       <button>Books </button>
       <button>Acc </button>
