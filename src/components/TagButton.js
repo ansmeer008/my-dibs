0ad997c9b4b6b0b693c092c5477f65d4d3b26b54
ansmeer008 <ansmeer008@gmail.com>
2023-03-15 21:09:41 +0900

    feat: use decendant selector

--- src/components/TagButton.js
+++ src/components/TagButton.js
@@ -1,15 +1,18 @@
 import classes from "./TagButton.module.css";
 
-export default function TagButton() {
+export default function TagButton({ data }) {
+  //클릭한 버튼의 이름을... 홈 페이지에 전달해서, map을 돌릴 때 filter로 돌려야 한다...
+  //클릭을 했을 때 서버에서 이미 filter한 데이터를 주면 좋겠지만,,, 가능하지 않을 수도 있으므로.,
+
   return (
     <div className={classes.container}>
-      <button className={classes.buttons}>Clothes </button>
-      <button className={classes.buttons}>Books </button>
-      <button className={classes.buttons}>Acc </button>
-      <button className={classes.buttons}>Home </button>
-      <button className={classes.buttons}>Electronics </button>
-      <button className={classes.buttons}>Culture </button>
-      <button className={classes.buttons}>Etc.</button>
+      <button>Clothes </button>
+      <button>Books </button>
+      <button>Acc </button>
+      <button>Home </button>
+      <button>Electronics </button>
+      <button>Culture </button>
+      <button>Etc.</button>
     </div>
   );
 }
