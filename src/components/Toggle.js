4dec348eb4adc0e1cf14303564187684aa36601c
ansmeer008 <ansmeer008@gmail.com>
2023-03-16 22:30:50 +0900

    feat: Toggle Component Markup #1

---
+++ src/components/Toggle.js
@@ -0,0 +1,22 @@
+import classes from "./Toggle.module.css";
+import { useState } from "react";
+import { useDarkMode } from "@/context/DarkModeContext";
+
+export default function Toggle() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <div className={classes.container}>
+      <button onClick={toggleDarkMode}>
+        <input
+          className={classes.checkbox}
+          type="checkbox"
+          defaultChecked={darkMode ? true : false}
+        ></input>
+        <span
+          className={`${classes.slider} ${darkMode ? "" : classes.unActive}`}
+        ></span>
+      </button>
+    </div>
+  );
+}
