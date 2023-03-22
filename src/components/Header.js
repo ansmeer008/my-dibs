342a7ad31c0e3833832a7dca48869271b54ad2b0
ansmeer008 <ansmeer008@gmail.com>
2023-03-22 17:01:12 +0900

    setting : divde path by isLogin (temp)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import { TbHandFinger } from "react-icons/tb";
 import Toggle from "./Toggle";
 
 export default function Header() {
+  const isLogin = true;
+
   return (
     <header className={classes.header}>
-      <Link href="/">
+      <Link href={isLogin ? "/feed" : "/"}>
         <div className={classes.title}>
           <h1>My Dibs!</h1>
           <div className={classes.icon}>
