13d422cae6e855f758d41cd58efdd02466f88050
ansmeer008 <ansmeer008@gmail.com>
2023-03-22 17:50:00 +0900

    test: apply dummy Data to postNewItem API #3

--- src/pages/newItem.js
+++ src/pages/newItem.js
@@ -5,11 +5,34 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import ConfirmModal from "@/components/modal/ConfirmModal";
 
+const DetailData = {
+  image:
+    "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  title: "무난한 티셔츠",
+  price: 18000,
+  tag: "clothes",
+  score: 4,
+  memo: "그냥 여기저기 입기 좋은 무난한 티셔츠",
+  url: "www.gwliee.com",
+};
+
+//TODO : 더미 데이터 변경 필요
+//TODO : 리다이렉션 추가 필요
+
 export default function NewItem() {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
 
-  function confirmHandler() {
+  async function confirmHandler() {
+    const response = await fetch("/api/new-item", {
+      method: "POST",
+      body: JSON.stringify(DetailData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
     setIsOpen((prev) => !prev);
   }
 
