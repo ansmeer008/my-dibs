caf621fe0f958057554a01c81c5d38ea074ea23a
ansmeer008 <ansmeer008@gmail.com>
2023-03-24 23:21:19 +0900

    feat: create extract password hook

---
+++ src/lib/api-helpers.js
@@ -0,0 +1,9 @@
+const sensitiveFields = ["password"];
+export function extractUser(user) {
+  if (!user) return null;
+  const obj = {};
+  Object.keys(user).forEach((key) => {
+    if (!sensitiveFields.includes(key)) obj[key] = user[key];
+  });
+  return obj;
+}
