9090f7918a6668eef0c59f5484d10715170b731a
ansmeer008 <ansmeer008@gmail.com>
2023-03-24 20:08:32 +0900

    feat: fill blank space with 'add some items!' #3

--- src/components/items/ItemList.js
+++ src/components/items/ItemList.js
@@ -4,17 +4,23 @@ import classes from "./ItemList.module.css";
 export default function ItemList({ items }) {
   return (
     <ul className={classes.list}>
-      {items.map((item) => {
-        return (
-          <FeedItem
-            key={item.id}
-            id={item.id}
-            image={item.image}
-            title={item.title}
-            price={item.price}
-          />
-        );
-      })}
+      {items.length === 0 ? (
+        <div className={classes.blankText}>
+          <span>Add Some Items!</span>
+        </div>
+      ) : (
+        items.map((item) => {
+          return (
+            <FeedItem
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              price={item.price}
+            />
+          );
+        })
+      )}
     </ul>
   );
 }
