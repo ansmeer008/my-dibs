5f7d3e454d2ac1f0f26743f74905d38db56cd048
ansmeer008 <ansmeer008@gmail.com>
2023-03-24 23:22:38 +0900

    feat: create middlewares #3

---
+++ src/middlewares/index.js
@@ -0,0 +1,3 @@
+export { default as database } from "./database.js";
+export { default as all } from "./all.js";
+export { default as session } from "./session.js";
---
+++ src/middlewares/passport.js
@@ -0,0 +1,30 @@
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import { Strategy as LocalStrategy } from "passport-local";
+import { findUserById, findUserByUsername } from "../db/index";
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+// passport#160
+passport.deserializeUser((req, id, done) => {
+  findUserById(req.db, id).then(
+    (user) => done(null, user),
+    (err) => done(err)
+  );
+});
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "username", passReqToCallback: true },
+    async (req, username, password, done) => {
+      const user = await findUserByUsername(req.db, username);
+      if (user && (await bcrypt.compare(password, user.password)))
+        done(null, user);
+      else done(null, false, { message: "username or password is incorrect" });
+    }
+  )
+);
+
+export default passport;
