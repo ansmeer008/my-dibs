f6075d3ecb83483ce8b6bbcc1ae1f58602113aa7
ansmeer008 <ansmeer008@gmail.com>
2023-04-04 16:28:48 +0900

    feat: set userId in useEffect

--- src/components/items/FeedItem.js
+++ src/components/items/FeedItem.js
@@ -3,11 +3,13 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function FeedItem({ id, image, title, price }) {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    setUserId(localStorage.getItem("userId"));
+  }, []);
   return (
-    <Link
-      href={`/detail/${localStorage.getItem("userId")}/${id}`}
-      className={classes.item}
-    >
+    <Link href={`/detail/${userId}/${id}`} className={classes.item}>
       <img
         src="https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dCUyMHNoaXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
         className={classes.image}
